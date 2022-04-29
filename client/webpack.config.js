67c463b1e5b577e9207a0a2035dbde160dae88a7
agmorrow <morrow.austin5@gmail.com>
2022-04-29 14:11:43 -0600

    fixed errors

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,17 +3,18 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
+    // Entry point for files
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js',
-     
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
+    // Output for our bundles
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
@@ -22,20 +23,22 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Text Editor'
+        title: 'JATE'
       }),
-       // Injects our custom service worker
-       new InjectManifest({
+     
+      // Injects our custom service worker
+      new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
+
       // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Text Editor',
-        short_name: 'jate',
-        description: 'editor!',
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Take notes in the browser!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: '/',
@@ -51,6 +54,7 @@ module.exports = () => {
     ],
 
     module: {
+      // CSS loaders
       rules: [
         {
           test: /\.css$/i,
